f157df8525835fac0c3d0145032a4393d2a30cdf
Bowie Chen <bowiechen@users.noreply.github.com>
2017-10-02 21:59:22 -0700

    Imports, Javadoc

--- app/src/main/java/dreamteam/focus/server/DatabaseConnector.java
+++ app/src/main/java/dreamteam/focus/server/DatabaseConnector.java
@@ -1,39 +1,124 @@
 package dreamteam.focus.server;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import dreamteam.focus.Profile;
+import dreamteam.focus.ProfileInSchedule;
+import dreamteam.focus.Schedule;
+
 /**
  * Created by bowie on 10/2/17.
  */
 
 public class DatabaseConnector {
-    public bool createProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean createProfile(Profile profile) {
+    }
 
-    public bool removeProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean removeProfile(Profile profile) {
+    }
 
-    public bool updateProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean updateProfile(Profile profile) {
+    }
 
-    public bool activateProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean activateProfile(Profile profile) {
+    }
 
-    public bool deactivateProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean deactivateProfile(Profile profile) {
+    }
 
-    public HashMap<String, Integer> getNotificationsForProfile(Profile profile) {}
+    /**
+     * @param profile
+     * @return a map of application names and the number of missed notifications
+     */
+    public HashMap<String, Integer> getNotificationsForProfile(Profile profile) {
+    }
 
-    public bool addSchedule(Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean addSchedule(Schedule schedule) {
+    }
 
-    public bool removeSchedule(Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean removeSchedule(Schedule schedule) {
+    }
 
-    public bool updateSchedule(Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean updateSchedule(Schedule schedule) {
+    }
 
-    public bool addProfileToSchedule(ProfileInSchedule pis, Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean addProfileToSchedule(ProfileInSchedule pis, Schedule schedule) {
+    }
 
-    public bool removeProfileToSchedule(ProfileInSchedule pis, Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean removeProfileToSchedule(ProfileInSchedule pis, Schedule schedule) {
+    }
 
-    public bool activateSchedule(Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean activateSchedule(Schedule schedule) {
+    }
 
-    public bool deactivateSchedule(Schedule schedule) {}
+    /**
+     * @param profile
+     * @return true on success; false on fail
+     */
+    public Boolean deactivateSchedule(Schedule schedule) {
+    }
 
-    public HashMap<String, Integer> getNotificationsForSchedule(Schedule schedule) {}
+    /**
+     * @param schedule
+     * @return a map of application names and the number of missed notifications
+     */
+    public HashMap<String, Integer> getNotificationsForSchedule(Schedule schedule) {
+    }
 
-    public Array<Profile> getProfiles() {}
+    /**
+     * @return an array of all the profiles present in the database
+     */
+    public ArrayList<Profile> getProfiles() {
+    }
 
-    public Array<Profile> getSchedules() {}
+    /**
+     * @return an array of all the schedules present in the database
+     */
+    public ArrayList<Profile> getSchedules() {
+    }
 }
