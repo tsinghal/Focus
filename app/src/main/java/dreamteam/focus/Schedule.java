a51ff07794f24d41b7bd89935f2bcd9e7d72e113
Bowie Chen <bowiechen@users.noreply.github.com>
2017-10-14 14:26:00 -0700

    Finalized classes for global use

--- app/src/main/java/dreamteam/focus/Schedule.java
+++ app/src/main/java/dreamteam/focus/Schedule.java
@@ -1,8 +1,57 @@
 package dreamteam.focus;
 
+import java.util.ArrayList;
+
 /**
  * Created by bowie on 10/2/17.
  */
 
 public class Schedule {
+    private String name;
+    private ArrayList<ProfileInSchedule> calendar;
+    private boolean active;
+
+    public Schedule(String name, ArrayList<ProfileInSchedule> calendar, boolean active) {
+        this.name = name;
+        this.calendar = calendar;
+        this.active = active;
+    }
+
+    public Schedule(String name, ArrayList<ProfileInSchedule> calendar) {
+        this.name = name;
+        this.calendar = calendar;
+        this.active = false;
+    }
+
+    public Schedule(String name) {
+        this.name = name;
+        this.calendar = new ArrayList<>();
+        this.active = false;
+    }
+
+    public ArrayList<ProfileInSchedule> getCalendar() {
+        return calendar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void addProfileToCalendar(ProfileInSchedule pis) {
+        calendar.add(pis);
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
 }
