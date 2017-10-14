2709ddda8d49e1a3499fe2917aa58ff1c202f57f
aaravmalpani <malpani@usc.edu>
2017-10-14 15:29:13 -0700

    Updates

--- app/src/main/java/dreamteam/focus/Profile.java
+++ app/src/main/java/dreamteam/focus/Profile.java
@@ -1,8 +1,55 @@
 package dreamteam.focus;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 /**
  * Created by bowie on 10/2/17.
  */
 
 public class Profile {
+    private String name;
+    private ArrayList<String> blockedApps;
+    private Date activateNowForTime;
+    private boolean active;
+    private ArrayList<String> blockedNotifications;
+
+    public Profile(String name, ArrayList<String> blockedApps, ArrayList<String> blockedNotifications) {
+        this.name = name;
+        this.blockedApps = blockedApps;
+        this.blockedNotifications = blockedNotifications;
+        active = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<String> getApps() {
+        return blockedApps;
+    }
+
+//    public void setBlockedApps(ArrayList<String> blockedApps) {
+//        this.blockedApps = blockedApps;
+//    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public ArrayList<String> getBlockedNotifications() {
+        return blockedNotifications;
+    }
+
+    public void addBlockedNotifications(String notification) {
+        this.blockedNotifications.add(notification);
+    }
 }
--- app/src/main/java/dreamteam/focus/Schedule.java
+++ app/src/main/java/dreamteam/focus/Schedule.java
@@ -1,8 +1,35 @@
 package dreamteam.focus;
 
 /**
- * Created by bowie on 10/2/17.
+ * Created by aarav on 10/13/17.
  */
 
 public class Schedule {
+
+    private String name;
+    private boolean active;
+
+
+    public Schedule(String name) {
+        this.name = name;
+        active = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+
 }
