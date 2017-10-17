3c38e4050acaf33a77e170af373cbd38ce783c7a
Bowie Chen <bowiechen@users.noreply.github.com>
2017-10-16 23:38:25 -0700

    Final commit before the deadline

--- app/src/main/java/dreamteam/focus/server/BackgroundService.java
+++ app/src/main/java/dreamteam/focus/server/BackgroundService.java
@@ -357,11 +357,11 @@ public class BackgroundService extends NotificationListenerService {
             if (oldBlockedApps.removeAll(blockedApps) || (blockedApps.size() == 0)) { // returns true of something is removed
                 for (String app : oldBlockedApps) {
                     // notify user about missed notifications
-                    int tmp = db.getNotificationsCountForApp(app);
-                    Log.d("db.getNotifCount = ", "" + tmp);
-                    // TODO: 10/16/17 getNotificationCount needs verification
-                    sendNotification(NOTIFICATION_ID_UNSEEN_NOTIFICATIONS + (int) (Math.random() * 1000),
-                            "You have unseen notifications from " + getAppNameFromPackage(app));
+                    if (db.getNotificationsCountForApp(app) != 0) {
+                        // TODO: 10/16/17 getNotificationCount needs verification
+                        sendNotification(NOTIFICATION_ID_UNSEEN_NOTIFICATIONS + (int) (Math.random() * 1000),
+                                "You have unseen notifications from " + getAppNameFromPackage(app));
+                    }
                 }
             }
         }
