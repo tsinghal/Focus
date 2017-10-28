a1fa8ab91edce8a9917102e16dee1291b5b5e0ee
Bowie Chen <bowiechen@users.noreply.github.com>
2017-10-28 15:53:02 -0700

    Added helper function for test

--- app/src/androidTest/java/dreamteam/focus/server/BackgroundServiceTest.java
+++ app/src/androidTest/java/dreamteam/focus/server/BackgroundServiceTest.java
@@ -1,7 +1,6 @@
 package dreamteam.focus.server;
 
 import android.support.test.InstrumentationRegistry;
-import android.support.test.espresso.Espresso;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.ViewInteraction;
@@ -36,10 +35,7 @@ import dreamteam.focus.Schedule;
 import dreamteam.focus.client.MainActivity;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -52,115 +48,81 @@ import static org.hamcrest.Matchers.allOf;
  * Created by bowie on 10/21/17.
  * Complete documentation in testing doc.
  */
-@SuppressWarnings({"FieldCanBeLocal", "unused"})
 public class BackgroundServiceTest {
-    private static final String WHATSAPP = "com.whatsapp";
-    private static final String FACEBOOK_MESSENGER = "com.facebook.orca";
-    private static final String BAD_SCHEDULE_NAME = "dfsjdofiewoiheegeg";
     private static final String GOOD_SCHEDULE_NAME = "test123";
-    private static final String ANONYMOUS_SCHEDULE = "AnonymousSchedule";
     private static final String NOTIFICATION_TITLE = "User Alert";
-    private Profile profile1;
-    private Profile profile2;
-    private Profile profile3;
-
-    private ProfileInSchedule pis1;
-    private ProfileInSchedule pis2;
-    private ProfileInSchedule pis3;
-
-    private Schedule schedule1;
-    private Schedule schedule2;
-    private Schedule schedule3;
-    private Schedule anonymousSchedule;
-    private Schedule goodSchedule;
-
-    private ArrayList<String> appBlacklist1;
-    private ArrayList<String> appBlacklist2;
-    private ArrayList<String> appBlacklist3;
-
-    private ArrayList<Repeat_Enum> enum1;
-    private ArrayList<Repeat_Enum> enum2;
-    private ArrayList<Repeat_Enum> enum3;
-
-    private ArrayList<ProfileInSchedule> cal1;
-    private ArrayList<ProfileInSchedule> cal2;
-    private ArrayList<ProfileInSchedule> cal3;
+    private static final long DELAY_MILLIS = 250;
 
     private DatabaseConnector db;
 
-    private int version = -1;
-
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
 
     @Before
     public void setUp() throws Exception {
         db = new DatabaseConnector(InstrumentationRegistry.getTargetContext());
-        db.clear();
 
-        appBlacklist1 = new ArrayList<>();
+        ArrayList<String> appBlacklist1 = new ArrayList<>();
         appBlacklist1.add("com.facebook.orca");
         appBlacklist1.add("com.whatsapp");
         appBlacklist1.add("com.facebook.katana");
-        appBlacklist2 = new ArrayList<>();
+        ArrayList<String> appBlacklist2 = new ArrayList<>();
         appBlacklist2.add("com.facebook.orca");
-        appBlacklist3 = new ArrayList<>();
+        ArrayList<String> appBlacklist3 = new ArrayList<>();
         appBlacklist3.add("com.facebook.orca");
         appBlacklist3.add("com.whatsapp");
 
-        enum1 = new ArrayList<>();
+        ArrayList<Repeat_Enum> enum1 = new ArrayList<>();
         enum1.add(Repeat_Enum.MONDAY);
         enum1.add(Repeat_Enum.WEDNESDAY);
         enum1.add(Repeat_Enum.FRIDAY);
         enum1.add(Repeat_Enum.SUNDAY);
-        enum2 = new ArrayList<>();
+        ArrayList<Repeat_Enum> enum2 = new ArrayList<>();
         enum2.add(Repeat_Enum.TUESDAY);
         enum2.add(Repeat_Enum.WEDNESDAY);
         enum2.add(Repeat_Enum.THURSDAY);
-        enum3 = new ArrayList<>();
+        ArrayList<Repeat_Enum> enum3 = new ArrayList<>();
         enum3.add(Repeat_Enum.MONDAY);
         enum3.add(Repeat_Enum.SATURDAY);
         enum3.add(Repeat_Enum.SUNDAY);
 
-        profile1 = new Profile("profile1", appBlacklist1);
-        profile2 = new Profile("profile2", appBlacklist2);
-        profile3 = new Profile("profile3", appBlacklist3);
+        Profile profile1 = new Profile("profile1", appBlacklist1);
+        Profile profile2 = new Profile("profile2", appBlacklist2);
+        Profile profile3 = new Profile("profile3", appBlacklist3);
 
         db.createProfile(profile1);
         db.createProfile(profile2);
         db.createProfile(profile3);
 
 
-        pis1 = new ProfileInSchedule(profile1,
+        ProfileInSchedule pis1 = new ProfileInSchedule(profile1,
                 new Date(Constants.TIME_1000), new Date(Constants.TIME_1200), enum1);
-        pis2 = new ProfileInSchedule(profile2,
+        ProfileInSchedule pis2 = new ProfileInSchedule(profile2,
                 new Date(Constants.TIME_0000), new Date(Constants.TIME_0400), enum2);
-        pis3 = new ProfileInSchedule(profile3,
+        ProfileInSchedule pis3 = new ProfileInSchedule(profile3,
                 new Date(Constants.TIME_0800), new Date(Constants.TIME_1200), enum3);
 
-        cal1 = new ArrayList<>();
+        ArrayList<ProfileInSchedule> cal1 = new ArrayList<>();
         cal1.add(pis1);
-        cal2 = new ArrayList<>();
+        ArrayList<ProfileInSchedule> cal2 = new ArrayList<>();
         cal2.add(pis1);
         cal2.add(pis2);
-        cal3 = new ArrayList<>();
+        ArrayList<ProfileInSchedule> cal3 = new ArrayList<>();
         cal3.add(pis1);
         cal3.add(pis2);
         cal3.add(pis3);
 
-        schedule1 = new Schedule("schedule1", cal1);
-        schedule2 = new Schedule("schedule2", cal2);
-        schedule3 = new Schedule("schedule3", cal3);
-        anonymousSchedule = new Schedule(ANONYMOUS_SCHEDULE);
-        goodSchedule = new Schedule(GOOD_SCHEDULE_NAME, cal3);
+        Schedule schedule1 = new Schedule("schedule1", cal1);
+        Schedule schedule2 = new Schedule("schedule2", cal2);
+        Schedule schedule3 = new Schedule("schedule3", cal3);
+        Schedule goodSchedule = new Schedule(GOOD_SCHEDULE_NAME, cal3);
 
 
         db.addSchedule(schedule1);
         db.addSchedule(schedule2);
+        db.addSchedule(schedule3);
         db.addSchedule(goodSchedule);
-
-
-        version = db.getDatabaseVersion();
     }
 
     @Test
@@ -169,7 +131,7 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -179,7 +141,7 @@ public class BackgroundServiceTest {
         appCompatButton.perform(click());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -196,16 +158,16 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction time=onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        ViewInteraction time = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
         time.perform(setTime());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -244,7 +206,7 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -254,7 +216,7 @@ public class BackgroundServiceTest {
         appCompatButton.perform(click());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -271,12 +233,12 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction time=onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        ViewInteraction time = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
         time.perform(setTime());
 
 
@@ -329,7 +291,7 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -339,7 +301,7 @@ public class BackgroundServiceTest {
         appCompatButton.perform(click());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -356,12 +318,12 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction time=onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        ViewInteraction time = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
         time.perform(setTime());
 
 
@@ -411,7 +373,7 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -421,7 +383,7 @@ public class BackgroundServiceTest {
         appCompatButton.perform(click());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -438,12 +400,12 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        ViewInteraction time=onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
+        ViewInteraction time = onView(withClassName(Matchers.equalTo(TimePicker.class.getName())));
         time.perform(setTime());
 
 
@@ -452,7 +414,7 @@ public class BackgroundServiceTest {
         appCompatButton4.perform(click());
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -469,7 +431,7 @@ public class BackgroundServiceTest {
         // The recommended way to handle such scenarios is to use Espresso idling resources:
         // https://google.github.io/android-testing-support-library/docs/espresso/idling-resource/index.html
         try {
-            Thread.sleep(4000);
+            Thread.sleep(DELAY_MILLIS);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
@@ -518,20 +480,22 @@ public class BackgroundServiceTest {
         };
     }
 
-    public static ViewAction setTime() {
+    private static ViewAction setTime() {
         return new ViewAction() {
             @Override
             public void perform(UiController uiController, View view) {
-                TimePicker tp =  (TimePicker) view;
-                int hour=tp.getHour();
-                int min=tp.getMinute();
+                TimePicker tp = (TimePicker) view;
+                int hour = tp.getHour();
+                int min = tp.getMinute();
                 tp.setHour(hour);
-                tp.setMinute(min+11);
+                tp.setMinute(min + 11);
             }
+
             @Override
             public String getDescription() {
                 return "Set the passed time into the TimePicker";
             }
+
             @Override
             public Matcher<View> getConstraints() {
                 return ViewMatchers.isAssignableFrom(TimePicker.class);
@@ -539,28 +503,22 @@ public class BackgroundServiceTest {
         };
     }
 
+    /**
+     * Activates instant profile for a specified time frame and a certain time until deactivation.
+     *
+     * @param profile   profile to be activated for length minutes.
+     * @param length    length of instant profile activation, in minutes
+     * @param timeUntil time until profile deactivation, in minutes
+     */
+    private void activateInstantProfile(Profile profile, int length, int timeUntil) throws Exception {
+        if (length < timeUntil) throw new Exception("length < timeUntil");
+        db.activateProfile(new ProfileInSchedule(profile,
+                new Date(new Date().getTime() - (length - timeUntil) * 60 * 1000),
+                new Date(new Date().getTime() + timeUntil * 60 * 1000)));
+    }
+
     @After
     public void tearDown() throws Exception {
-
-    }
-
-    @Test
-    public void onStartCommand() throws Exception {
-
-    }
-
-    @Test
-    public void onNotificationPosted() throws Exception {
-
-    }
-
-    @Test
-    public void onNotificationRemoved() throws Exception {
-
-    }
-
-    @Test
-    public void tick() throws Exception {
-
+        db.clear();
     }
 }
